64f72245ea2ff3b16b7fbebc0ef7579b837d7498
ViiktorioB <vbiserov@cifpfbmoll.eu>
2023-10-11 17:34:51 +0200

    first commit

---
+++ Caraocruz/script.js
@@ -0,0 +1,9 @@
+var yo = 1;
+var j2 = 2;
+
+var resultado = Math.floor(Math.random()*2)+1; 
+if(resultado == yo){
+    console.log("El resultado es "+ resultado+ " has ganado!!");
+}else{
+    console.log("El resultado es "+ resultado+ " has perdido :(")
+}
